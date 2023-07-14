4b90113fd007fbdb3561018ecbc7a3113a4d5fa8
kmavit <kimavit@gmail.com>
2023-07-14 23:12:18 +0300

    numbers

--- numbers.py
+++ numbers.py
@@ -3,4 +3,4 @@ digit3 = num % 10
 digit2 = (num // 10) % 10
 digit1 = num // 100
 print("Сумма цифр =", digit1 + digit2 + digit3)
-print("Произведение цифр =", digit1 + digit2 + digit3)
+print("Произведение цифр =", digit1 * digit2 * digit3)
